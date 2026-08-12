27eb662fd35c0557495a49ef60a2035f29fe0438
Soyeon Park <iovethdus@gmail.com>
2026-08-12 09:43:47 +0900

    Update script.js

--- 99_formdatabase/js/script.js
+++ 99_formdatabase/js/script.js
@@ -1,40 +1,27 @@
 const scriptURL =
-"https://script.google.com/macros/s/AKfycbwGKCWBPFsKP9n7SaLHQ3hhSwVxMhXJyVNIwojBAcQVtLxVJIL8VT91o0RHrYwAM_E6Xw/exec";
+  "https://script.google.com/macros/s/AKfycbwGKCWBPFsKP9n7SaLHQ3hhSwVxMhXJyVNIwojBAcQVtLxVJIL8VT91o0RHrYwAM_E6Xw/exec";
 
-const form = document.getElementById("contactForm");
+const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
 
-form.addEventListener("submit", function(e) {
-
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     fetch(scriptURL, {
         method: "POST",
         body: new FormData(form)
     })
-
-    .then(function(response) {
-
-        // 입력값 초기화
+    .then((response) => {
         form.reset();
 
-        // 성공 메시지 표시
-        alert("성공적으로 제출되었습니다.");
         msg.textContent = "성공적으로 제출되었습니다.";
 
-        // 5초 뒤 메시지 제거
-        setTimeout(function() {
+        setTimeout(() => {
             msg.textContent = "";
         }, 5000);
-
     })
-
-    .catch(function(error) {
-
+    .catch((error) => {
         console.error("Error!", error.message);
-
         msg.textContent = "제출 중 오류가 발생했습니다.";
-
     });
-
 });
