ad2785a84c78376e09615438b00e0dafb301acea
Soyeon Park <iovethdus@gmail.com>
2026-08-12 09:41:50 +0900

    Update script.js

--- 99_formdatabase/js/script.js
+++ 99_formdatabase/js/script.js
@@ -19,6 +19,7 @@ form.addEventListener("submit", function(e) {
         form.reset();
 
         // 성공 메시지 표시
+        alert("성공적으로 제출되었습니다.");
         msg.textContent = "성공적으로 제출되었습니다.";
 
         // 5초 뒤 메시지 제거
